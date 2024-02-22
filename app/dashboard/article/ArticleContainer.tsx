0fbaf7b66719515959162862bbe11c94a692769a
Suraj Vishwakarma <surajvishwakarma625@gmail.com>
2024-02-22 19:54:39 +0530

    improve analytics

--- app/dashboard/article/ArticleContainer.tsx
+++ app/dashboard/article/ArticleContainer.tsx
@@ -4,33 +4,41 @@ import Link from "next/link";
 
 const ArticleContainer = ({ article, type }: any) => {
   if (type === "published") {
-    console.log(article)
-    return <Box>
-      <Text variant="primary-text">{article.title}</Text>
-      <Divider variant="primary-divider"/>
-    </Box>;
+    console.log(article);
+    return (
+      <Box>
+        <Text variant="primary-text" pt={3}>
+          {article.title}
+        </Text>
+        <Divider variant="primary-divider" />
+      </Box>
+    );
   }
   if (type === "scheduled") {
-    return <Box>
-      <Flex justifyContent="space-between" alignItems="center">
-    <Text variant="primary-text">{article.title}</Text>
-    <Link href={`article/${article.article_id}`}>
-    <Button variant="primary-button">Edit</Button>
-    </Link>
-    </Flex>
-    <Divider variant="primary-divider"/>
-  </Box>;
+    return (
+      <Box>
+        <Flex justifyContent="space-between" alignItems="center">
+          <Text variant="primary-text">{article.title}</Text>
+          <Link href={`article/${article.article_id}`}>
+            <Button variant="primary-button">Edit</Button>
+          </Link>
+        </Flex>
+        <Divider variant="primary-divider" />
+      </Box>
+    );
   }
   if (type === "draft") {
-    return <Box>
-    <Flex justifyContent="space-between" alignItems="center">
-  <Text variant="primary-text">{article.title}</Text>
-  <Link href={`article/${article.article_id}`}>
-  <Button variant="primary-button">Edit</Button>
-  </Link>
-  </Flex>
-  <Divider variant="primary-divider"/>
-</Box>;
+    return (
+      <Box>
+        <Flex justifyContent="space-between" alignItems="center">
+          <Text variant="primary-text">{article.title}</Text>
+          <Link href={`article/${article.article_id}`}>
+            <Button variant="primary-button">Edit</Button>
+          </Link>
+        </Flex>
+        <Divider variant="primary-divider" />
+      </Box>
+    );
   }
 };
 
